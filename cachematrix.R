a5cbcc03bc8d71962c0b6989b94fca69a4281e7f
Nancy Sonnenfeld <fiddlin_nancy@verizon.net>
2015-01-22 20:08:59 -0500

    edited file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Function: makeCacheMatrix ;
+##Purpose: A square matrix (set) and its inverse (setinverse) are stored ;
+##         as functions in the global environment.     ;
+##         They are called in the functions get and getinverse;
+##         Finally, the function makeInverse calls all 4 functions;
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x =matrix()) {
+  m <- NULL
+  set <- function(i) {
+    x <<- i
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set= set, get = get, setinverse = setinverse,
+       getinverse = getinverse)
 }
 
+##Function: cacheSolve ;
+##Purpose: Retrieves the matrix and its inverse  ;
+##         and returns the inverse     ;
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
